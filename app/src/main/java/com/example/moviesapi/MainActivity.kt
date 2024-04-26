a29d18c1ae5380455d4ee7c379a1708ec5ba5375
ernestoo-v <evillarperez@gmail.com>
2024-04-26 17:49:03 +0200

    h

--- app/src/main/java/com/example/moviesapi/MainActivity.kt
+++ app/src/main/java/com/example/moviesapi/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.moviesapi
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +17,7 @@ import com.example.moviesapi.ui.screens.mainScreen.MainScreenViewModel
 import com.example.moviesapi.ui.theme.MoviesAPITheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
